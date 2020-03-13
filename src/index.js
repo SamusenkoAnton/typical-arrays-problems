0b74f18a2810c9e3fde9f4c7f66af92029e0036e
Anton <samusenkoanton@yandex.ru>
2020-03-13 20:58:05 +0300

    feat add: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array){
+    if (array == null || array == undefined || array.length == 0){
+        return 0;
+    }
+    let min = array[0];
+
+    for (let i = 0; i < array.length;i++){
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+    console.log(min);
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array){
+    if (array == null || array == undefined || array.length == 0){
+        return 0;
+    }
+    let max = array[0];
+
+    for (let i = 0; i < array.length;i++){
+        if(array[i] > max){
+            max = array[i];
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array){
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    if (array == null || array == undefined || array.length == 0){
+        return 0;
+    }
+    array = array.reduce(reducer)/array.length;
+    return array;
 }
